719ddc07f7aac67b2cc8f442dc0d9613f1d841c0
Clément Bertin <clement.bertin@supinfo.com>
2024-01-25 09:45:51 +0100

    ajout affichage

---
+++ project/Elements.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from math import sqrt
+
+class Elements:
+    def __init__(self,id:int,box_w:float,box_h:float,x:float,y:float,canva) :
+        self._player_id = id
+        self._color = "red" if self._player_id == 1 else "blue"
+        self._box_dim=(box_w,box_h)
+        self._coords=(x,y)
+        self._radius=(self._box_dim[1]*0.9)/2
+        self._side=sqrt((self._radius**2)*2)
+        self._canva=canva
+        
+    def get_player(self):
+        return self.__player_id
+
+    def get_box(self):
+        return (self.__coords[1]/self._box_dim[1],self.__coords[0]/self._box_dim[0])
+    
+    def get_coords(self):
+        return self._coords
+    
+    def get_box_size(self):
+        return self._box_dim
+    
+    
+    def set_box(self,ligne,col):
+        self._coords=(col*self._box_dim[0],ligne*self._box_dim[1])
+        
+    def set_coords(self,x,y):
+        self._coords=(x,y)
+        
+    def change_player(self):
+        self._player_id=3-self._player_id
+        self._color = "red" if self._player_id == 1 else "blue"
+        
+        
+class Pawn(Elements):
+    def __init__(self, id,canva,box_w,box_h,x,y):
+        super().__init__(id,box_w,box_h,x,y,canva)
+        
+        
+    def draw(self):
+        self._canva.create_oval(self._coords[0]-self._side,self._coords[1]-self._side,self._coords[0]+self._side,self._coords[1]+self._side,fill=self._color,outline='black', width=2   )
+        
+        
+class Ring(Elements):
+    def __init__(self, id,canva,box_w,box_h,x,y):
+        super().__init__(id,box_w,box_h,x,y,canva)
+
+        
+    def draw(self):
+        self._canva.create_oval(self._coords[0]-self._side,self._coords[1]-self._side,self._coords[0]+self._side,self._coords[1]+self._side,fill='',outline=self._color, width=5 )
+            
+
+    
+    
--- project/game.py
+++ project/game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from Elements import Ring,Pawn
 
 class Game:
     def __init__(self) -> None:
@@ -9,12 +10,18 @@ class Game:
         self.__root.configure(bg="black")
         
         
-        self.__canva_width=700
-        self.__canva_height=700
+        self.__canva_width=self.__root.winfo_screenwidth()/2.3
+        self.__canva_height=self.__root.winfo_screenheight()/1.5
         self.__box_heiht=self.__canva_height/20
         self.__box_width=self.__canva_width/12
         self.__canva=Canvas(self.__root, width=self.__canva_width, height=self.__canva_height,highlightthickness=0)
         self.__canva.pack()
+        ring=Ring(2,self.__canva,self.__box_width,self.__box_heiht,0,0)
+        ring.set_box(8,6)
+        self.__ring_list=[ring]
+        pawn=Pawn(1,self.__canva,self.__box_width,self.__box_heiht,0,0)
+        pawn.set_box(3,5)
+        self.__pawn_list=[pawn]
         
         self.update()
         self.__root.mainloop()
@@ -41,7 +48,10 @@ class Game:
             self.__canva.create_line(self.__canva_width*i+(-1)**i*4*self.__box_width,18*self.__box_heiht,self.__canva_width*i+(-1)**i*4*self.__box_width,2*self.__box_heiht,fill="black",width=2)
             self.__canva.create_line(self.__canva_width*i+(-1)**i*5*self.__box_width,19*self.__box_heiht,self.__canva_width*i+(-1)**i*5*self.__box_width,1*self.__box_heiht,fill="black",width=2)
         self.__canva.create_line(self.__canva_width*i+(-1)**i*6*self.__box_width,18*self.__box_heiht,self.__canva_width*i+(-1)**i*6*self.__box_width,2*self.__box_heiht,fill="black",width=2)
-            
+        for elem in self.__pawn_list:
+            elem.draw()
+        for elem in self.__ring_list:
+            elem.draw()
             
             
 Game()
